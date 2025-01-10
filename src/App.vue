2b61a2f10e17060c9bf67b41e6bd5e310b2ac8fc
modyu-liu <2216879042@qq.com>
2025-01-10 13:31:55 +0800

    [UPDATE]

--- src/App.vue
+++ src/App.vue
@@ -34,7 +34,7 @@ onMounted(() => {
   </p>
   
   <p style="text-align: center; font-size: 1.5em;">
-      <a href="" target="_blank">[Paper]</a>&nbsp;&nbsp;&nbsp;&nbsp;
+      <a href="http://arxiv.org/abs/2501.05177" target="_blank">[Paper]</a>&nbsp;&nbsp;&nbsp;&nbsp;
       <a href="https://github.com/modyu-liu/FaceMe" target="_blank">[Code]</a>&nbsp;&nbsp;&nbsp;&nbsp;
       <a href="https://huggingface.co/datasets/thomas2modyu/FaceMe" target="_blank">[Dataset]</a>&nbsp;&nbsp;&nbsp;&nbsp;
       <!-- <a href="" target="_blank">[Demo]</a> -->
